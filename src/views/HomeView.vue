16928c20ffc4636c3e06af7c4844237610d2d277
olevatorr <otis.coding@gmail.com>
2024-06-25 03:47:25 +0800

    adjust/ favicon和github連結跳轉

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,6 @@
 <template>
   <div class="container mx-auto p-4 boder-box flex flex-col justify-center items-center">
     <div class="text-6xl">Welcome!</div>
-    <a href="https://github.com/olevatorr" class="text-2xl text-gray-600 tracking-wide hover:text-gray-900 transition-colors mt-5 animate-bounce"><i class="fa-brands fa-github mr-2"></i>olevatorr</a>
+    <a href="https://github.com/olevatorr" target="_blank" class="text-2xl text-gray-600 tracking-wide hover:text-gray-900 transition-colors mt-5 animate-bounce"><i class="fa-brands fa-github mr-2"></i>olevatorr</a>
   </div>
 </template>
